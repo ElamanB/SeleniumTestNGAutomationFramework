d74e28f852349e05df9c2b3d3b61248f5c47a113
ElamanB <166842582+ElamanB@users.noreply.github.com>
2024-05-01 11:42:37 -0500

    Update ConflictTests.java

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -15,6 +15,6 @@ public class ConflictTests {
     @Test
     public void test2() {
         String message = "Welcome to USA!";
-        Assert.assertTrue(message.toLowerCase().contains("USA"));
+        Assert.assertTrue(message.toUpperCase().contains("USA"));
     }
 }
